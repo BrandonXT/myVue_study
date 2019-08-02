e9a04d3d34ce85bd79182a0fcb48bfb3677fabc1
Brandon_X <1499672553@qq.com>
2019-08-02 11:26:17 +0800

    路由滚动事件

--- my-projects/pizza-app/src/components/Home.vue
+++ my-projects/pizza-app/src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <button @click="goToMenu" class="btn btn-success">跳转</button>
+  <div class="row">
+    <div id="home" class="col-sm-12 text-center">
+      <div class="background">
+        <h1>欢迎大家品尝pizza</h1>
+        <h2>这里有非常美味的pizza</h2>
+        <button @click="goToMenu" class="btn btn-success">跳转</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,19 +14,39 @@ export default {
   methods: {
     goToMenu() {
       //跳转到上次浏览的页面
-    //   this.$router.go(-1);
+      //   this.$router.go(-1);
 
-    //指定跳转的地址
-    // this.$router.replace('/menu');
+      //指定跳转的地址
+      // this.$router.replace('/menu');
 
-    //指定跳转路由的名字下
-    // this.$router.replace({name:'menuLink'});
+      //指定跳转路由的名字下
+      // this.$router.replace({name:'menuLink'});
 
-    //通过push进行跳转
-    // this.$router.push('/menu');
-    this.$router.push({name:'menuLink'});
+      //通过push进行跳转
+      // this.$router.push('/menu');
+      this.$router.push({ name: "menuLink" });
     }
   }
 };
 </script>
+
+<style>
+  #home{
+    background: url('../assets/4a6181231b3c43e4539cdd6dff292740.png');
+    /* background: url('../assets/logo.png'); */
+    height: 85vh;
+    padding: 10%;
+  }
+  h1,h2{
+    margin:6%;
+  }
+  .background{
+    background: #eee;
+    opacity: 0.8;
+    max-width: 70vw;
+    margin: 0 auto;
+    padding:20px 0;
+  }
+</style>
+
 
--- my-projects/pizza-app/src/main.js
+++ my-projects/pizza-app/src/main.js
@@ -7,7 +7,22 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes,
-  mode: 'history' //去掉那个 # 号
+  mode: 'history', //去掉那个 # 号
+  //配置路由滚动
+  scrollBehavior(to,from,savedPosition){
+    //x，y轴距离
+    // return {x:0,y:500}
+    
+    //根据选择器跳转
+    // return {selector:'.btn'}
+
+    //利用savePosition记住上次滚动地点
+    // if(savedPosition){
+    //   return savedPosition
+    // }else{
+    //   return{x:0,y:0}
+    // }
+  }
 })
 
 //全局守卫
