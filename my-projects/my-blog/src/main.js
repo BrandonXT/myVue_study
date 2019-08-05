cb309a980a44ddb6d0c1a968b2b6e9b14a7fcdb6
Brandon_X <1499672553@qq.com>
2019-08-05 10:30:06 +0800

    自定义函数

--- my-projects/my-blog/src/main.js
+++ my-projects/my-blog/src/main.js
@@ -8,9 +8,35 @@ Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
+// 自定义指令
+Vue.directive('rainbow', {
+  bind(el, binding, vnode) {
+    el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+  }
+})
+//自定义传参模板(el为对象，binding为传递的参数 )
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth == "1260px";
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth == "560px";
+      // el.style.background = "#ccc";
+    }
+    //自定义使用修饰符
+    if (binding.arg == 'column') {
+      el.style.background = '#6677cc';
+      el.style.padding = '30px';
+    }
+  }
+})
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
