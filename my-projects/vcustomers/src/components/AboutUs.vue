21596dae804b1307d023e339a45637c49f08f9f5
Brandon_X <1499672553@qq.com>
2019-08-07 09:00:33 +0800

    （vcustomers）路由与导航栏

---
+++ my-projects/vcustomers/src/components/AboutUs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="aboutus">
+    ABOUTUS
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+  data () {
+    return {
+   
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- my-projects/vcustomers/src/main.js
+++ my-projects/vcustomers/src/main.js
@@ -2,12 +2,33 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+//引入路由
+import Customers from './components/Customers.vue'
+import AboutUs from './components/AboutUs.vue'
+
+//设置路由
+const router = new VueRouter({
+  mode:"history",
+  routes:[
+    {path:"/",component:Customers},
+    {path:"/aboutus",component:AboutUs},
+  ],
+  base:__dirname
+})
+
+
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components:{App},
+  template: '<App/>',
+  router,
 })
